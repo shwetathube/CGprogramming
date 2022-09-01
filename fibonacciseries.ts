9ec5298fb03aefb65039c3623366a7622c1c55e3
LAPTOP-CMGGEGSS\Shweta <shwetathube22@gmail.com>
2022-09-01 12:01:27 +0530

    first program in CG

---
+++ fibonacciseries.ts
@@ -0,0 +1,11 @@
+let n:number=10;
+let n1:number=1;
+console.log(n1);
+let n2:number=1;
+console.log(n2);
+let n3:number;
+for(let i=3;i<10;i++){
+    n3=n1+n2;
+    console.log(n3);
+    n1=n2;
+    n2=n3;}
